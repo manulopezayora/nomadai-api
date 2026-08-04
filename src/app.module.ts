3442146bc553d3bed67f407b30a7a41f359ca30e
Manu <manulopezayora1985@protonmail.com>
2026-08-04 16:27:02 +0200

    chore: clean code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
